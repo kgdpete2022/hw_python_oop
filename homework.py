b92234ffb08cd75026a064b3f0b5cee668adfaa0
Пётр Шопин <mail4dev2022@gmail.com>
2022-08-19 20:01:39 +0200

    Исправлено условие проверки наличия типа тренировки в словаре (переформатировано без else)

--- homework.py
+++ homework.py
@@ -120,8 +120,7 @@ def read_package(workout_type: str, data: list) -> Training:
                  'WLK': SportsWalking}
     if workout_type in trainings:
         return trainings[workout_type](*data)
-    else:
-        print('Указан неверный тип тренировки')
+    print('Указан неверный тип тренировки')
 
 
 def main(training: Training) -> None:
